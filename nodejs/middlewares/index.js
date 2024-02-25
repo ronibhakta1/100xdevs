c1db9a4b438d0e45b4c32b8dadbf31934d8db954
Roni bhakta <ronibhakta1@gmail.com>
2024-02-25 19:31:29 +0530

    middlewares

--- nodejs/middlewares/index.js
+++ nodejs/middlewares/index.js
@@ -1,33 +1,57 @@
+// part two cleaner way 
 
 const express = require('express');
 const app = express();
 const port =3000;
 
-//ugly way for creating middleware
-app.get('/health-checkup',(req,res)=>{
-    //do health checks here 
+
+function usercheck(req,res,next){
     const username = req.headers.username;
     const password = req.headers.password;
-    const kidneyId = req.query.kidneyId;
-    const kidneyId1 = req.query.kidneyId1;
-
-    console.log(kidneyId)
-    if (username != "Roni" && password != "pass") {
+    if (username != "Roni" || password != "pass") {
         res.status(403).json({
             msg:"user doesnt exist"
         })   
         return;     
+    }else{
+        next();
     }
-    if (kidneyId != 1 || kidneyId1 != 2 ) {
+}
+
+function kidneycheck(req,res,next){
+    const kidneyId = req.query.kidneyId;
+    const kidneyId1 = req.query.kidneyId1;
+    if (kidneyId === 1 || kidneyId1 === 2 ) {
         res.status(411).json({
-            msg: "Wrong inputs"
+            msg: "Wrong inputs",
+        
         })        
         return;
+    }else{
+        next();
     }
+}
+
+//cleaner way for creating middleware
+
+app.get('/health-checkup',usercheck,kidneycheck,(req,res)=>{
     // do something with kidney here
     res.json({
         msg :"your kidney is fine"
     })
 })
+let requesthits= 0;
+// introducing app.use here the second way of using middleware 
+function middlewarecheck1(req,res,next){
+    requesthits++;
+    console.log(requesthits);
+    next();
+}
+app.use(middlewarecheck1);
 
-app.listen(3000);
+app.get("/request-hits",(req,res)=>{
+    res.json({
+        msg:'hi there from the request-hits'
+    })
+})
+app.listen(port);
