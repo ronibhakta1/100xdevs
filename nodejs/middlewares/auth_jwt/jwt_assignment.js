521d4158841b4680eb911059e0920d3db4c98651
Roni bhakta <ronibhakta1@gmail.com>
2024-07-12 18:24:23 +0530

    completed with auth

--- nodejs/middlewares/auth_jwt/jwt_assignment.js
+++ nodejs/middlewares/auth_jwt/jwt_assignment.js
@@ -9,22 +9,72 @@ const zod = require("zod");
 const emailSchema = zod.string().email();
 const passwordSchema = zod.string().min(6);
 
-function signJwt(username, password){
+function signJwt(username, password) {
     const usernameResponse = emailSchema.safeParse(username);
     const passwordResponce = passwordSchema.safeParse(password);
-    if(!usernameResponse.success || !passwordResponce.success){
+    if (!usernameResponse.success || !passwordResponce.success) {
         return null;
-    }else{
-        const signature = jwt.sign(username,jwtPassword)
+    } else {
+        const signature = jwt.sign(username, jwtPassword)
         return signature;
     }
 }
 
-const out =signJwt("ronigmail.com", "6hdfsgdgd");
-console.log(out)
+const out = signJwt("roni@gmail.com", "6hdfsgdgd");
+if (out == !null) {
+    console.log("Token = " + out);
+} else {
+    console.log("Token status = " + out);
+}
+
+
 // app.listen(port);
 
 // Here we have created an funtion which creates a jwt token using jsonwebtoken, and a zod lib to check wheather the user is giving us the right inputs 
 //after that we have successfully created a jwt token using the key and the inputs
 
-function decodeJwt(token)
+function decodeJwt(token) {
+    const decode = jwt.decode(token);
+    // console.log(decode);
+    if (decode) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const decodeStatus = decodeJwt(out);
+console.log("decode status = " + decodeStatus);
+
+// here we decoded the jwt token
+
+// this logic works if the token is correct, if the token gets wrong it throws an execption
+
+// function verifyJwt(token1){
+//     const verified = jwt.verify(token1, jwtPassword);
+//     console.log(verified);
+//     if (verified) {
+//         return true;
+//     }else{
+//         return false;
+//     }
+// }
+
+// const verifyStatus = verifyJwt(out);
+// console.log(verifyStatus);
+
+// to overcome this we use try catch 
+
+function verifyJwt(token1) {
+
+    try {
+        const verified = jwt.verify(token1, jwtPassword);
+        // console.log(verified);
+        return true;
+    } catch (error){
+        return false;
+    }
+}
+
+const verifyStatus = verifyJwt(out);
+console.log("verify status = " + verifyStatus);
