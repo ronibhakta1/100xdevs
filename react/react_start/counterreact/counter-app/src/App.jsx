46ac3e2f4e64b559319ff837b57803ad0951af7d
Roni bhakta <ronibhakta1@gmail.com>
2024-09-17 22:20:15 +0530

    react jsx counter app

---
+++ react/react_start/counterreact/counter-app/src/App.jsx
@@ -0,0 +1,31 @@
+//hook 
+import {useState} from 'react';
+import PropTypes from 'prop-types';
+
+//state,component 
+// let state = {
+//   count : 0
+// }
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <CustonButton count={count} setCount={setCount}></CustonButton>
+      <CustonButton count={count +1 } setCount={setCount}></CustonButton>
+      <CustonButton count={count - 1 } setCount={setCount}></CustonButton>
+      <CustonButton count={count* 2} setCount={setCount}></CustonButton>
+    </div>
+  )
+}
+function CustonButton(props){
+
+  function onClickHandeler(){
+    props.setCount(props.count + 1);
+  }
+
+  return <button onClick={onClickHandeler}>
+    Counter {props.count}
+    </button>
+}
+
+export default App
